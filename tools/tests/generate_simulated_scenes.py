a3e279b2e42af466e4f5e6beb5e0dfce7e3c5e0e
zhenzhen.xiang <zhenzhen.xiang@lingzhitech.com>
2020-07-23 13:19:35 +0800

    put the random selecting object data outside the random scene generator

--- tools/tests/generate_simulated_scenes.py
+++ tools/tests/generate_simulated_scenes.py
@@ -1,5 +1,6 @@
 import json
 import pickle
+import random
 from pathlib import Path
 from multiprocessing import Pool
 
@@ -9,13 +10,13 @@ from tools.tests.random_scene_generation import SceneGenerator
 
 
 class Generator(object):
-    def __init__(self, data_folder, output_folder, objects_data):
+    def __init__(self, data_folder, output_folder):
         # create scene generator
-        self.sg = SceneGenerator(data_folder, output_folder, objects_data)
+        self.sg = SceneGenerator(data_folder, output_folder)
 
-    def run(self, data_dict):
+    def run(self, input_data):
         # generate a scene
-        self.sg.generate_scene(data_dict)
+        self.sg.generate_scene(input_data['label_data'], input_data['object_data'])
 
         # save scene cloud to .bin file
         self.sg.save_scene_cloud_to_file()
@@ -36,7 +37,7 @@ if __name__ == '__main__':
 
     data_folder = '/home/data/deecamp/DeepCamp_Lidar'
     objects_folder = '/home/data/deecamp/DeepCamp_Lidar/objects'
-    label_file = Path(data_folder).joinpath('labels_filer/val_filter.txt')
+    label_file = Path(data_folder).joinpath('labels_filer/train_filter.txt')
 
     output_scenes_folder = Path(data_folder).joinpath('sim_scenes')
     output_scenes_folder.mkdir(exist_ok=True)
@@ -60,10 +61,25 @@ if __name__ == '__main__':
 
     logging.info('Total labeled scenes: {}'.format(len(labels)))
 
+    # generate input data
+    input_data = list()
+    # -- select a part or all labels for generation
+    selected_labels = labels
+
+    # -- generate object sample data
+    subset_size = 50
+    for label_data in selected_labels:
+        sampled_objects = list()
+        for obj in objects_data:
+            class_name = obj['class_name']
+            samples = random.sample(obj['objects'], subset_size)
+            sampled_objects.append({'class_name': class_name, 'objects': samples})
+        input_data.append({'label_data': label_data, 'object_data': sampled_objects})
+
     # generate and save results
-    g = Generator(data_folder, output_scenes_folder, objects_data)
+    g = Generator(data_folder, output_scenes_folder)
     p = Pool()
-    generated_labels = p.map(g.run, labels)
+    generated_labels = p.map(g.run, input_data)
 
     # save labels to file
     output_label_file = output_label_folder.joinpath('sim_scene.txt')
